06182b16f6e7343c3f4059fe82311d5b7b35cc73
Idris <Idbalorg@gmail.com>
2023-02-16 15:35:32 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// let cash = 2000;
+// let expenses = 4000;
+// console.log(cash === expenses);
+
+// let myAge = 19;
+
+// (myAge >= 18) ? console.log('you are eligible to vote') : console.log('you are not elligible to vote');
+
+let num = 12;
+let pwr = 1;
+while (pwr <= num/2) {
+  pwr *= 2;
+}
+
+console.log("The number " + pwr + " is the power of 2 nearest to " + num +".");
